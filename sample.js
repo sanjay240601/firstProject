a309d3bfc815bba6c0e54fbd991b43a68ba6f7ff
unknown <sanjay@digivalsolutions.com>
2022-11-16 12:06:14 +0530

    sample module

---
+++ sample.js
@@ -0,0 +1,18 @@
+const Joi = require('joi');
+const schema = Joi.object()
+  .keys({
+    name: Joi.string()
+      .min(3)
+      .max(40)
+      .required(),
+    age: Joi.number()
+      .integer()
+      .min(16)
+  })
+const data = {
+  name: 'sanjay',
+  age:16
+};
+
+const result = schema.validate(data);
+console.log(result);
